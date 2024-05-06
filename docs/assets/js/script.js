f6cafffc1570902470cdb1b3f469adf92a2bc73c
lkalabis <kalabis.lukas@gmail.com>
2024-05-07 00:15:56 +0200

    update request page

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -13,10 +13,19 @@ document.querySelector('.sun-moon-switcher').addEventListener('click', () => {
 });
 
 // if dark mode is enabled, show the sun icon, else show the moon icon
-if (isDarkMode() === 'enabled') {
+console.log(isDarkMode());
+if (isDarkMode()) {
     document.querySelector('.sun').style.display = 'block';
-    document.querySelector('.moon').style.display = 'none';
+    document.querySelector('.moon').style.display = '';
+    // add the dark class to the body if is not already there
+    if (!document.body.classList.contains('dark')) {
+        document.body.classList.add('dark');
+    }
 } else {
     document.querySelector('.sun').style.display = 'none';
     document.querySelector('.moon').style.display = 'block';
+    // remove the dark class from the body if is there
+    if (document.body.classList.contains('dark')) {
+        document.body.classList.remove('dark');
+    }
 }
